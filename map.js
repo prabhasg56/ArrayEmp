190db59836a10428f90c89cb1ce4afc2cec9e157
PRABHAKAR MANDAL <prabhakar.mandal.20.2@mountblue.tech>
2022-07-14 11:07:41 +0000

    Handle undefined return

--- map.js
+++ map.js
@@ -1,23 +1,25 @@
-function impMap(ele,cb){
-    let res=[];
-    if((Array.isArray(ele))===true){
-        
-        if(((ele===undefined)&&(cb===undefined))||(cb===undefined)||(ele.length===0)||typeof cb!=='function') {
-    
+function impMap(ele, cb) {
+    let res = [];
+    if ((Array.isArray(ele)) === true) {
+
+        if (((ele === undefined) && (cb === undefined)) || (cb === undefined) || (ele.length === 0) || typeof cb !== 'function') {
+
             return res;
         }
-        else{
-            for(let i=0;i<ele.length;i++){
-
-                res.push(cb(ele[i]));
-               
+        else {
+            for(let i = 0; i < ele.length; i++){
+                if(ele[i]===undefined){
+                    continue;
+                }
+                let element=parseInt(ele[i]);
+                res.push(cb(element,i,ele));
             }
-        } 
+        }
     }
-    else{
+    else {
         return res;
-     }
-   return res;
+    }
+    return res;
 }
 
-module.exports=impMap;
+module.exports = impMap;
