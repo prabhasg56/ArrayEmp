8c743315a93198d7149f6f06666a291d74eb9840
PRABHAKAR MANDAL <prabhakar.mandal.20.2@mountblue.tech>
2022-07-14 11:03:55 +0000

    Implement depth

--- flatten.js
+++ flatten.js
@@ -1,27 +1,35 @@
-function impFlatten(ele){
-    let res=[];
-    if((Array.isArray(ele))===true){
-        
-        if((ele===undefined)||(ele.length===0)) {
-    
-            return res;
-        }
-        else{
-            let joinEle=ele.join(',');
-            //console.log(joinEle);
-            let splitArr=joinEle.split(',');
-            //console.log(splitArr);
-            for(let i=0;i<splitArr.length;i++){
-               res.push(parseInt(splitArr[i]));
-               
-            }
-        } 
+function impFlatten(ele, depth) {
+  let res = [];
+  if ((Array.isArray(ele)) === true) {
+
+    if ((ele=== undefined) || (ele.length === 0)) {
+
+      return res;
     }
     else{
+        if(depth===undefined){
+          depth=1;
+        }
+        for (let i = 0; i < ele.length; i++) {
+            if (Array.isArray(ele[i]) && depth > 0) {
+                //console.log(ele[i])
+                res = res.concat(impFlatten(ele[i], depth - 1));
+                //console.log(res);
+            }
+            else if (ele[i] === undefined) {
+                continue;
+            }
+            else {
+                res.push(ele[i]);
+                //console.log(res)
+            }
+        }
         return res;
-     }  
-    return res; 
-
+      }  
+    }
+  else {
+    return res;
+  }
 }
+module.exports = impFlatten;
 
-module.exports=impFlatten;
