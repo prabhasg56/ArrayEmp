780198dc311432573abfc66ab6d3bb246da11d2c
PRABHAKAR MANDAL <prabhakar.mandal.20.2@mountblue.tech>
2022-07-14 11:13:41 +0000

    Pass fourth Argument and implement addition

--- reduce.js
+++ reduce.js
@@ -1,25 +1,29 @@
-function impReduce(ele,cb,stValue){
-   
-    if((Array.isArray(ele))===true){
-        
-        if(((ele===undefined)&&(cb===undefined))||(cb===undefined)||(ele.length===0)||typeof cb!=='function') {
-    
-            return [];
-        }
-        else{
-            for(let i=0;i<ele.length;i++){
-              if(stValue===undefined){
-                stValue=ele[0];
-              }
-              stValue=cb(stValue,ele[i]);
-               
-            }
-        } 
+function impReduce(ele, cb, stValue) {
+
+  if ((Array.isArray(ele)) === true) {
+
+    if (((ele === undefined) && (cb === undefined)) || (cb === undefined) || (ele.length === 0) || typeof cb !== 'function') {
+
+      return [];
     }
-    else{
-        return [];
-     }
-   return stValue;
+    else {
+      if (stValue === undefined) {
+         stValue = ele[0];
+        for (let i = 1; i < ele.length; i++) {
+          stValue = cb(stValue, ele[i], i, ele);
+        }
+      }
+      else {
+        for (let i = 0; i < ele.length; i++) {
+          stValue = cb(stValue, ele[i], i, ele);
+        }
+      }
+    }
+  }
+  else {
+    return [];
+  }
+  return stValue;
 }
 
-module.exports=impReduce;
+module.exports = impReduce;
