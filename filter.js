1153269ac1dbcd8d33c34c2dc9ea42fbd6160242
PRABHAKAR MANDAL <prabhakar.mandal.20.2@mountblue.tech>
2022-07-14 11:00:02 +0000

    Add third parameter

--- filter.js
+++ filter.js
@@ -1,25 +1,24 @@
-function impFilter(ele,cb){
-    let res=[];
-    if((Array.isArray(ele))===true){
-        
-        if(((ele===undefined)&&(cb===undefined))||(cb===undefined)||(ele.length===0)||typeof cb!=='function') {
-    
+function impFilter(ele, cb) {
+    let res = [];
+    if ((Array.isArray(ele)) === true) {
+
+        if (((ele === undefined) && (cb === undefined)) || (cb === undefined) || (ele.length === 0) || typeof cb !== 'function') {
+
             return res;
         }
-        else{
-            for(let i=0;i<ele.length;i++){
-               const r=cb(ele[i]);
-               if(r===true){
-                res.push(ele[i]);
-               }
-               
+        else {
+            for (let i = 0; i < ele.length; i++) {
+                const r = cb(ele[i],i,ele);
+                if (r === true) {
+                    res.push(ele[i]);
+                }
             }
-        } 
+        }
     }
-    else{
+    else {
         return res;
-     }
+    }
     return res;
 }
 
-module.exports=impFilter;
+module.exports = impFilter;
