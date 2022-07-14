4db7b9aefc868094cde391083598b3da11e89dbb
PRABHAKAR MANDAL <prabhakar.mandal.20.2@mountblue.tech>
2022-07-14 11:16:01 +0000

    Update Test/testFlatten.js

--- Test/testFlatten.js
+++ Test/testFlatten.js
@@ -1,4 +1,6 @@
-const nestedArray = [1, [2], [[3]], [[[4]]]];
+const nestedArray = [1, [2],[[3]],[[[5]]]];
 const impFlatten=require('../flatten.js')
+const depth=3
+const result=impFlatten(nestedArray,depth);
 
-console.log(impFlatten(nestedArray));
+console.log(result);
